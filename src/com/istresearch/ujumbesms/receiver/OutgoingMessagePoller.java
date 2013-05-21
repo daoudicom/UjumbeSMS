40c527446f08d9368acbf2b7cfb985fe12567e83
baracudda <ryan.fischbach@istresearch.com>
2013-05-21 11:54:14 -0400

    first attempt to convert to UjumbeSMS, but Settings activity fails to run.
    Signed-off-by: baracudda <ryan.fischbach@istresearch.com>

---
+++ src/com/istresearch/ujumbesms/receiver/OutgoingMessagePoller.java
@@ -0,0 +1,16 @@
+package com.istresearch.ujumbesms.receiver;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+import com.istresearch.ujumbesms.App;
+
+public class OutgoingMessagePoller extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        App app = (App) context.getApplicationContext();
+        app.checkOutgoingMessages();        
+    }
+}
