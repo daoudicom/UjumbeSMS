4fb477d9ae2bdb71e3f5a491bca1a34ea1a0c0ec
arahman2 <abdullah.rahmanyar@gmail.com>
2013-05-30 14:16:02 -0400

    Modified manifest and receiver to be able to receive broadcast from ujumbecache (questions). uSMS is able to receive the data, however, it does not do anything to the data yet, it does not forward it to the users/phones

--- src/com/istresearch/ujumbesms/receiver/UjumbeCacheReceiver.java
+++ src/com/istresearch/ujumbesms/receiver/UjumbeCacheReceiver.java
@@ -4,6 +4,7 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Toast;
 
 public class UjumbeCacheReceiver extends BroadcastReceiver {
@@ -11,6 +12,7 @@ public class UjumbeCacheReceiver extends BroadcastReceiver {
 	private Bundle extras;
 	private String test = "recieved from ujumbe cache";
 	private String fail = "nothing in the intent";
+	private String TAG = "myApp";
 	@Override
 	public void onReceive(Context context, Intent intent) {
 		extras = intent.getExtras();
@@ -18,6 +20,7 @@ public class UjumbeCacheReceiver extends BroadcastReceiver {
 			String message = extras.getString("message");
 			String phoneNumber = extras.getString("number");
 			Toast.makeText(context, test, Toast.LENGTH_SHORT).show();
+			Log.i(TAG, "received from UjumbeCache" + message +"  "+phoneNumber);
 		}
 		else{
 			Toast.makeText(context, fail, Toast.LENGTH_SHORT).show();
